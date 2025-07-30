b60bc723cab6c25238260ab3d4db06a8d4023278
Elyor Yadgarov <eyodgorov@gmail.com>
2025-07-30 16:03:55 -0400

    Updates

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -5,8 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter count of your Vehicle");
-            var count = int.Parse(Console.ReadLine());
-            var vehicle = VehicleFactory.CreateVehicle(count);
+            // var count = int.Parse(Console.ReadLine());
+            int option;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out option))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter a valid option");
+            }
+            var vehicle = VehicleFactory.CreateVehicle(option);
             vehicle.Drive();
         }
     }
